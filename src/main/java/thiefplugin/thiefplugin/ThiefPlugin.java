8cc485753f7098115cb7c10141ded8596f2c076c
korovkaK22 <115235686+korovkaK22@users.noreply.github.com>
2022-11-18 19:27:34 +0200

    Initial commit

---
+++ src/main/java/thiefplugin/thiefplugin/ThiefPlugin.java
@@ -0,0 +1,17 @@
+package thiefplugin.thiefplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThiefPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
