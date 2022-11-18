a07919265292ecb4128aeeac402ea2f92ff7e946
korovkaK22 <115235686+korovkaK22@users.noreply.github.com>
2022-11-18 19:35:22 +0200

    1

--- src/main/java/events/Events.java
+++ src/main/java/events/Events.java
@@ -1,4 +1,30 @@
 package events;
 
-public class Events {
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Evoker;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerPickupArrowEvent;
+import thiefplugin.thiefplugin.ThiefPlugin;
+
+public class Events implements Listener {
+    ThiefPlugin plugin;
+
+
+    public Events(ThiefPlugin plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+        this.plugin = plugin;
+    }
+
+
+    //Посадить на хрюшу
+    @EventHandler
+    public void thiefPlayer(PlayerPickupArrowEvent event) {
+
+
+    }
+
+
 }
